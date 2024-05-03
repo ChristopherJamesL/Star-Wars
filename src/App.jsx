3a426b178dac956d05ab572c3602d246db9a7b8c
ChristopherL <anbchris@gmail.com>
2024-05-03 12:24:40 -0400

    async fetch

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,51 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            creatures: [],
+            creatures: {
+                results: []
+            },
             searchfield: ''
         }
+        console.log('construct');
     }
 
-    componentDidMount() {
-        this.setState({ creatures: creatures });
-        console.log('check')
-    }
+    async componentDidMount() {
+        const urls = [
+            'https://swapi.py4e.com/api/planets/',
+            'https://swapi.py4e.com/api/species/',
+            'https://swapi.py4e.com/api/films/'   
+        ];
+        const response = await fetch('https://swapi.py4e.com/api/people/');
+        const data = await response.json();
+        const [ planets, species, films ] = await Promise.all(urls.map(url => 
+            fetch(url).then(resp => resp.json())
+        ));
+        this.setState({ creatures: data });
+        console.log('componentDidMount')
+        // this.setState({creatures.results.homeworld: planets });
+   }
+
+//        async componentDidMount() {
+//         const response = await fetch('https://swapi.py4e.com/api/people/');
+//         const data = await response.json();
+//         this.setState({ creatures: data});
+//         console.log('componentDidMount')
+//    }   
+   
+    // componentDidMount() {
+    //     fetch('https://swapi.py4e.com/api/people/')
+    //         .then(response => {
+    //             return response.json();
+    //         })
+    //         .then(data => {
+    //             this.setState({ creatures: data })
+    //         });
+    // }
+    
+    // componentDidMount() {
+    //     this.setState({ creatures: creatures });
+    //     console.log('componentDidMount')
+    // }
 
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
@@ -33,10 +69,8 @@ class App extends Component {
         console.log('render');
         return (
             <div className='tc '>
-                
-                    <h1 className='f1 yellow'>Star Wars the Final Frontier</h1>
-                    <Searchbox  searchChange={this.onSearchChange}/>
-                
+                <h1 className='f1 yellow'>Star Wars the Final Frontier</h1>
+                <Searchbox  searchChange={this.onSearchChange}/>
                 <CardList creatures={filteredCreatures}/>
             </div>
         );
